403be180d0caf992bd7c49611906722b23edcca3
Lê Huy Mạnh Tân <81731577+lehuymanhtan@users.noreply.github.com>
2024-05-26 16:43:01 +0700

    Update rm paywall forbes.js

--- rm paywall forbes.js
+++ rm paywall forbes.js
@@ -1,6 +1,5 @@
 // ==UserScript==
 // @name         fobes unblock?
-// @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
